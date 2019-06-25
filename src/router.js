95710f76af4113f1d2b46cedeefc5fab28e45c55
Taekleee <catalina.morales.r@usach.cl>
2019-06-25 01:06:20 -0400

    vistas y componentes grafos

--- src/router.js
+++ src/router.js
@@ -3,12 +3,17 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Generos from './views/Generos.vue'
 import Artistas from './views/Artistas.vue'
+import Grafos from './views/Grafos.vue'
 import GeneroGrafico1 from './views/GeneroGrafico1.vue'
 import GeneroGrafico2 from './views/GeneroGrafico2.vue'
 import GeneroGrafico3 from './views/GeneroGrafico3.vue'
 import ArtistaGrafico1 from './views/ArtistaGrafico1.vue'
 import ArtistaGrafico2 from './views/ArtistaGrafico2.vue'
 import ArtistaGrafico3 from './views/ArtistaGrafico3.vue'
+import Grafo1 from  './views/Grafo1.vue'
+import Grafo2 from  './views/Grafo2.vue'
+import Grafo3 from  './views/Grafo3.vue'
+
 
 Vue.use(Router)
 
@@ -29,6 +34,11 @@ export default new Router({
       path: '/artistas',
       name: 'artistas',
       component: Artistas
+    },
+    {
+      path: '/grafos',
+      name: 'grafos',
+      component: Grafos
     },
 
     {
@@ -61,7 +71,20 @@ export default new Router({
       name: 'artistaGrafico3',
       component: ArtistaGrafico3
     },
-  
-
+    {
+      path: '/grafo1',
+      name: 'grafo1',
+      component: Grafo1
+    },
+    {
+      path: '/grafo2',
+      name: 'grafo2',
+      component: Grafo2
+    },
+    {
+      path: '/grafo3',
+      name: 'grafo3',
+      component: Grafo3
+    },
   ]
 })
